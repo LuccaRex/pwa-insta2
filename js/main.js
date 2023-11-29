78fb8d4f6814554399499281836199e24af6ee76
Lucca Paschoalim <luccafuso2017@gmail.com>
2023-11-28 23:56:37 -0400

    commit teste

--- js/main.js
+++ js/main.js
@@ -1,47 +1,23 @@
-// Verifica se o navegador suporta a API de câmera
-if (!('mediaDevices' in navigator) || !('getUserMedia' in navigator.mediaDevices)) {
-  alert('API de câmera não suportada no seu navegador!');
-}
+var video = document.querySelector('video');
 
-let capturedImage;
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream => {
+    video.srcObject = stream;
+    video.play();
+})
+.catch(error => {
+    console.log(error);
+})
 
-// Função para capturar a foto
-async function capturePhoto() {
-  const video = document.createElement('video');
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-  document.body.appendChild(video);
-  video.srcObject = stream;
-
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      video.play();
-      const canvas = document.createElement('canvas');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-      capturedImage = canvas.toDataURL('image/png');
-      stream.getTracks().forEach(track => track.stop());
-      document.body.removeChild(video);
-      resolve(capturedImage);
-    };
-  });
-}
-
-// Adiciona evento ao botão de captura
-document.getElementById('captureButton').addEventListener('click', async () => {
-  capturedImage = await capturePhoto();
-  const imageContainer = document.getElementById('imageContainer');
-  imageContainer.innerHTML = `<img src="${capturedImage}" alt="Captured Photo">`;
-});
-
-// Adiciona evento ao botão de postagem
-document.getElementById('postButton').addEventListener('click', () => {
-  if (capturedImage) {
-    // Aqui você pode implementar a lógica para postar a imagem, por exemplo, usando uma API ou serviço de backend.
-    // Neste exemplo, exibiremos um alerta com o caminho da imagem.
-    alert('Imagem postada: ' + capturedImage);
-  } else {
-    alert('Capture uma foto antes de postar!');
-  }
+document.querySelector('button').addEventListener('click', () => {
+    var canvas = document.querySelector('canvas');
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+    var link = document.createElement('a');
+    link.download = 'foto.png';
+    link.href = canvas.toDataURL();
+    link.textContent = 'Clique para baixar a imagem';
+    document.body.appendChild(link);
 });
